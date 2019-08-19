b9f64080208bdae781479d716f3353e6d70207a2
bluecake <bluekezhou@outlook.com>
2019-08-19 17:24:48 +0800

    摄像头接入及识别已完成

--- js/demo_util.js
+++ js/demo_util.js
@@ -60,10 +60,8 @@ function updateTryResNetButtonDatGuiCss() {
 function toggleLoadingUI(
     showLoadingUI, loadingDivId = 'loading', mainDivId = 'main') {
   if (showLoadingUI) {
-    document.getElementById(loadingDivId).style.display = 'block';
     document.getElementById(mainDivId).style.display = 'none';
   } else {
-    document.getElementById(loadingDivId).style.display = 'none';
     document.getElementById(mainDivId).style.display = 'block';
   }
 }
