9f1f0a1def080ecc1f028a20ebcc3c716863d281
bluecake <bluekezhou@outlook.com>
2019-08-19 16:02:02 +0800

    extract core code from tensorflow demo

--- camera.js
+++ camera.js
@@ -1,28 +1,10 @@
-/**
- * @license
- * Copyright 2018 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-import * as posenet from '@tensorflow-models/posenet';
-import dat from 'dat.gui';
-import Stats from 'stats.js';
-
-import {drawBoundingBox, drawKeypoints, drawSkeleton, isMobile, toggleLoadingUI, tryResNetButtonName, tryResNetButtonText, updateTryResNetButtonDatGuiCss} from './demo_util';
-
 const videoWidth = 600;
 const videoHeight = 500;
 const stats = new Stats();
+
+function isMobile(){
+  return false;
+}
 
 /**
  * Loads a the camera to be used in the demo
@@ -450,7 +432,7 @@ function detectPoseInRealTime(video, net) {
  * Kicks off the demo by loading the posenet model, finding and loading
  * available camera devices, and setting off the detectPoseInRealTime function.
  */
-export async function bindPage() {
+async function bindPage() {
   toggleLoadingUI(true);
   const net = await posenet.load({
     architecture: guiState.input.architecture,
